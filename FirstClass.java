e01490a443f0b789aaaf61e80f62c660bd1dcdb2
sasvathrn <rknisha91@gmail.com>
2019-09-25 22:07:27 +0530

    first commit

---
+++ FirstClass.java
@@ -0,0 +1,33 @@
+
+/**
+ * This is my first sample class to understand java programming learning.
+ *
+ * @author (R N Sasvath)
+ * @version (0.0.1)
+ */
+public class FirstClass
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class FirstClass
+     */
+    public FirstClass()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
